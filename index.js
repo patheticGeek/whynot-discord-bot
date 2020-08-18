aa8460a3b32e17f2a67f4fdefef92fd8e0e5f1b4
Pathetic Geek <geekpathetic@gmail.com>
2020-08-19 05:20:46 +0530

    :thumbsup: ping commands added

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const { clearLogs, logger } = require("./util/logger");
 
+const prefix = process.env.PREFIX;
 const client = new Discord.Client();
 
 client.on("ready", () => {
   logger(`Bot ready with prefix ${process.env.PREFIX}`);
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    message.channel.send("Pong.");
+  } else if (command === "beep") {
+    message.channel.send("Boop.");
+  } else if (command === "boop") {
+    message.channel.send("Beep.");
+  }
 });
 
 clearLogs();
