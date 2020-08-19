dae6be477cf0836119454854a6a097fefb7b5207
Pathetic Geek <geekpathetic@gmail.com>
2020-08-19 08:52:45 +0530

    dynamic commands

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 require("dotenv").config();
+const fs = require("fs");
 const Discord = require("discord.js");
 const { clearLogs, logger } = require("./util/logger");
 
 const prefix = process.env.PREFIX;
 const client = new Discord.Client();
+
+function setCommands(client) {
+  client.commands = new Discord.Collection();
+
+  const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+  }
+}
 
 client.on("ready", () => {
   logger(`Bot ready with prefix ${process.env.PREFIX}`);
@@ -15,18 +27,20 @@ client.on("message", (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (command === "ping") {
-    message.channel.send("Pong.");
-  } else if (command === "beep") {
-    message.channel.send("Boop.");
-  } else if (command === "boop") {
-    message.channel.send("Beep.");
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
   }
 });
 
 clearLogs();
 
 if (process.env.DISCORD_BOT_TOKEN) {
+  setCommands(client);
   client.login(process.env.DISCORD_BOT_TOKEN);
 } else {
   logger("Please add an env var DISCORD_BOT_TOKEN");
